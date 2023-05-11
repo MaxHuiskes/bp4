54794a703eb7f2c58507bb4a92986a84bc2ed92e
MaxHuiskes <max.huiskes@outlook.com>
2023-05-11 16:04:44 +0200

    code apperanse fix

--- app/src/main/java/com/example/porgamring/Tasks.java
+++ app/src/main/java/com/example/porgamring/Tasks.java
@@ -68,7 +68,7 @@ public class Tasks {
         }
 
         assert notificationManager != null;
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        notificationManager.notify(0 , notificationBuilder.build());
     }
 
 
